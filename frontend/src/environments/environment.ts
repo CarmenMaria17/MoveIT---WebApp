8095180a7c94cef52cc6265e0a7113ad316ca3fd
Sara <floreansara24@gmail.com>
2025-12-05 01:55:05 +0200

    Added map URL

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    arcgisMapUrl: "https://services7.arcgis.com/kdfqOEP2UUGv3hmO/arcgis/rest/services/SportsCenters_FeatureLayer/FeatureServer"
+  };
